103416df7eff6db00976ff9c41f4824522e937ed
Joseph Jenkins <92091383+JOSEPH-JENKINS@users.noreply.github.com>
2025-10-08 11:21:57 +0100

    Update ga.client.js

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -1,23 +1,21 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  // ensure gtag exists even if script hasn't loaded yet
-  // @ts-ignore
-  window.dataLayer = window.dataLayer || [];
-  // @ts-ignore
-  window.gtag = window.gtag || function(){ window.dataLayer.push(arguments as any); };
+  // make sure dataLayer and gtag exist
+  window.dataLayer = window.dataLayer || []
+  window.gtag = window.gtag || function(){ window.dataLayer.push(arguments) }
 
-  // fire initial page_view
-  const send = () => {
-    // @ts-ignore
+  function sendPageView() {
     window.gtag('event', 'page_view', {
       page_location: window.location.href,
       page_path: window.location.pathname + window.location.search,
       page_title: document.title
-    });
-  };
-  send();
+    })
+  }
 
-  // fire on every nuxt route change
+  // initial load
+  sendPageView()
+
+  // run on each route change
   nuxtApp.hook('page:finish', () => {
-    send();
-  });
-});
+    sendPageView()
+  })
+})
